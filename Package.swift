3384f8edb1d84b7eb34d31031d598c2d7e9e72a8
Sham Sundar <Sham.Sundar@costrategix.com>
2023-01-13 14:13:54 +0530

    Upgraded to the SDK version 4.11.1

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "DigitalReefSDK",
-            url: "https://github.com/Digita1Reef/iOSDRSDKSPM/releases/download/4.11.0/DigitalReefSDK_4.11.0.zip",
-            checksum: "dacc12a1cbd8fed1662640dbca66a7f337735557bf8cd5b009f8f463be3ea867"),
+            url: "https://github.com/Digita1Reef/iOSDRSDKSPM/releases/download/4.11.1/DigitalReefSDK_4.11.1.zip",
+            checksum: "fb127717a32d66c9e1a448c00bff00f51fbd2b3910a0d7ca3692f674360397c7"),
         .target(
             name: "iOSDRSDKSPM",
             dependencies: [
