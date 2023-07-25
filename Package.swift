85f9d4f6c5148ea59fb8456801b9926e7310c19b
Sham Sundar <Sham.Sundar@costrategix.com>
2023-07-25 18:12:30 +0530

    Updated the SDK to version 4.15.4

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "DigitalReefSDK",
-            url: "https://github.com/Digita1Reef/iOSDRSDKSPM/releases/download/4.15.3/DigitalReefSDK_4.15.3.zip",
-            checksum: "4efdfec45c19c3fbb174960545c6b763f80fdcfebc4feb2c85f6709c6d52403c"),
+            url: "https://github.com/Digita1Reef/iOSDRSDKSPM/releases/download/4.15.4/DigitalReefSDK_4.15.4.zip",
+            checksum: "7edfca747d936ceb0ed66341a1799ed7a5224cbc3be180c172544228c9e0f323"),
         .target(
             name: "iOSDRSDKSPM",
             dependencies: [
